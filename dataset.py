ab39b8e08378d58bd437cc5ec2ba3b33a7048794
Brian Li <39709270+bl21812@users.noreply.github.com>
2023-11-29 15:29:45 -0500

    indexing

--- dataset.py
+++ dataset.py
@@ -59,12 +59,12 @@ class RNAInputDataset(Dataset):
 
         # load, one-hot encode secondary structure
         if self.pretrain:
-            label = self.df['secondary_struct'][idx]
+            label = self.df['secondary_struct'].iloc[idx]
             label = [self.secondary_struct_encode[c] for c in label]
 
         # load, reactivities
         else:
-            label = self.df['reactivity'][idx]
+            label = self.df['reactivity'].iloc[idx]
 
         # pad label
         label += [0 for _ in range(pad_amount)]
