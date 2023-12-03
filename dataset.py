b5b343d2799fa27c5fedcc01015eb842d643688a
Brian Li <39709270+bl21812@users.noreply.github.com>
2023-12-02 20:37:49 -0500

    syntax

--- dataset.py
+++ dataset.py
@@ -41,7 +41,7 @@ class RNAInputDataset(Dataset):
 
         # SNR filter columns only exists in train data
         if snr_filter:
-            self.df = self.df[self.df['SN_filter']].reset_index()
+            self.df = self.df[self.df['SN_filter'] == True].reset_index()
 
         # put reactivities into one column if using them as labels
         if not pretrain:
